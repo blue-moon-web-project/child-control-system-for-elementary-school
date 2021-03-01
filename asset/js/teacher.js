6f771bcb48d94e149867e1f3b5b378fd8b979e39
Biruk_Kassaw <se.biruk.kassaw@gmail.com>
2021-03-01 11:14:14 +0300

    displaying teachers information on his page

--- asset/js/teacher.js
+++ asset/js/teacher.js
@@ -3,7 +3,7 @@ console.log(urlParams)
 const id = urlParams.get('id');
 console.log(id);
 
-db.collection('teacher').doc({ id: id }).get().then(teacher => {
+db.collection('teachers').doc({ id: id }).get().then(teacher => {
     console.log(teacher)
 
     var welcomeNameH4 = document.getElementById("welcomeNameH4");
@@ -11,6 +11,10 @@ db.collection('teacher').doc({ id: id }).get().then(teacher => {
 
     var idDisplay = document.getElementById("id");
     idDisplay.innerHTML = teacher.id
+
+    var name = document.getElementById("firstName");
+    name.value = teacher.name;
+
 
     var zone = document.getElementById("zone");
     zone.value = teacher.zone;
@@ -20,5 +24,43 @@ db.collection('teacher').doc({ id: id }).get().then(teacher => {
 
     var kebele = document.getElementById("kebele");
     kebele.value = teacher.kebele;
+
+    var gradeAndSubject = document.getElementById("gradeAndSubject");
+    gradeAndSubject.innerHTML = `Grade ${teacher.grade} ${teacher.subject}`
+
+    db.collection('grades').get().then(grades => {
+        console.log(grades)
+        var myStudents = [];
+
+        grades.forEach(grade => {
+            if (grade.studentGrade == teacher.grade) {
+                myStudents.push(grade)
+            }
+            grade.studentId = "ha/4386/21"
+            db.collection('grades').add(grade);
+
+        });
+        console.log(myStudents)
+        var gradesDisplayerDiv = document.getElementById("gradesDisplayerDiv");
+        var gradesDisplayerString = `<li class="list-group-item d-flex justify-content-between align-items-center active">
+        Student Id
+        <span class="">Grade</span>
+      </li>`
+        myStudents.forEach(displayStudent => {
+            // display the students here
+            gradesDisplayerString += `<li id="" class="list-group-item d-flex justify-content-between align-items-center grade">
+            ${displayStudent.studentId}
+            <input type="text" value="${displayStudent.thisSemesterGrades[1].value}">
+          </li>`
+        });
+
+        gradesDisplayerDiv.innerHTML = gradesDisplayerString
+        var saveChanges = document.getElementById("saveChanges");
+
+        saveChanges.addEventListener("click", function (e) {
+            
+        })
+
+      })
 })
 
