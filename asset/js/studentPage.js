f210c6e85ca4b7e0611e967efea1051d0491145c
Biruk_Kassaw <se.biruk.kassaw@gmail.com>
2021-03-01 04:59:51 +0300

    displaying the privious grades of the student

--- asset/js/studentPage.js
+++ asset/js/studentPage.js
@@ -1,9 +1,14 @@
 
 const urlParams = new URLSearchParams(window.location.search);
-const id = Number(urlParams.get('id'));
+console.log(urlParams)
+const id = urlParams.get('id');
 console.log(id);
 
-db.collection('students').doc({ id: "ha/8864/21" }).get().then(student => {
+
+
+
+
+db.collection('students').doc({ id: id }).get().then(student => {
     console.log(student)
     var welcomeNameH4 = document.getElementById("welcomeNameH4");
     welcomeNameH4.innerHTML = "Wellcome "+student.firstName + " " + student.lastName
@@ -36,7 +41,85 @@ db.collection('students').doc({ id: "ha/8864/21" }).get().then(student => {
     studentSaveChanges.addEventListener("click", function (e) {
     });
 
-    var thisSemesterGrades = student.thisSemesterGrades;
+    db.collection('grades').doc({ studentId: student.id }).get().then(grades => {
+      console.log(grades)
+      var thisSemesterGrades = grades.thisSemesterGrades;
+      console.log(thisSemesterGrades)
+
+      var maths = document.getElementById("maths")
+      maths.innerHTML = thisSemesterGrades[0].value
+
+      var english = document.getElementById("english")
+      english.innerHTML = thisSemesterGrades[1].value
+
+      var physics = document.getElementById("physics")
+      physics.innerHTML = thisSemesterGrades[2].value
+
+      var biology = document.getElementById("biology")
+      biology.innerHTML = thisSemesterGrades[3].value
+
+      var chemistry = document.getElementById("chemistry")
+      chemistry.innerHTML = thisSemesterGrades[4].value
+
+      var civics = document.getElementById("civics")
+      civics.innerHTML = thisSemesterGrades[6].value
+
+      var french = document.getElementById("french")
+      french.innerHTML = thisSemesterGrades[7].value
+
+      var sport = document.getElementById("sport")
+      sport.innerHTML = thisSemesterGrades[8].value
+
+    
+      var previousSemesterGrades = grades.previousSemesterGrades
+      
+      var previousGradesDispalayer = document.getElementById("previousGradesDispalayer")
+      var previousGradesString = `<h3 class="display-4 text-center m-5">previous semester Grades`
+      previousSemesterGrades.forEach(semesterGrade => {
+        console.log(semesterGrade);
+        previousGradesString += `<h3 class="text-center m-5">${semesterGrade.semester}</h3> <center><ul class="list-group" style="max-width:800px;">
+        <li class="list-group-item d-flex justify-content-between align-items-center active">
+          Subject
+          <span class="">Grade</span>
+        </li>
+        <li id="" class="list-group-item d-flex justify-content-between align-items-center">
+          Maths
+          <span class="" id="maths">${thisSemesterGrades[0].value}</span>
+        </li>
+        <li id="" class="list-group-item d-flex justify-content-between align-items-center">
+          English
+          <span class="" id="english">${thisSemesterGrades[1].value}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Biology
+          <span class="" id="biology">${thisSemesterGrades[2].value}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Physics
+          <span class="" id="physics">${thisSemesterGrades[3].value}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Chemistry
+          <span class="" id="chemistry">${thisSemesterGrades[4].value}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Civics
+          <span class="" id="civics">${thisSemesterGrades[6].value}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          French
+          <span class="" id="french">${thisSemesterGrades[7].value}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Sport
+          <span class="" id="sport">${thisSemesterGrades[8].value}</span>
+        </li>
+    </ul><center>`
+      });
+      previousGradesDispalayer.innerHTML = previousGradesString;
+    })
+
+   
 
   })
 
