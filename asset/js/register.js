a79b5fcff29ff507d8b5faa01ff14cf83ad89724
Biruk_Kassaw <se.biruk.kassaw@gmail.com>
2021-03-01 03:14:50 +0300

    displaying students this semister grade

--- asset/js/register.js
+++ asset/js/register.js
@@ -1,184 +1,108 @@
-(function () {
-    // all the code will be here  
-    const request = indexedDB.open('SchoolDatabase', 2);
-
-    request.onerror = (event) => {
-        console.error(`Database error: ${event.target.errorCode}`);
-    };
-     
-    // create the Contacts object store and indexes
-    request.onupgradeneeded = (event) => {
-        let db = event.target.result;
-
-        // create the Contacts object store 
-        // with auto-increment id
-        // let store = db.createObjectStore('Students', {
-        //     keyPath: "id",
-        //     autoIncrement: true
-        // });
-
-        let gradeStore = db.createObjectStore('Grades', {
-            keyPath: "id",
-        });
-
-    };
-
-    request.onsuccess = (event) => {
-        // add implementation here
-        const db = event.target.result;
-
-        const register = document.getElementById('submit_new_student');
-        
-        // new student Registrations
-        // getting students data from register.html and storing it into the db
-
-        
-
-        // var firstName = "Biruk";
-        // var lastName = "Kassaw";
-        // var dateOfBirth = new Date();
-        // var zone = "akaki kality ";
-        // var woreda = "woreda 6";
-        // var kebele = "7";
-        // var houseNo = "653";
-        // var poBox = "678";
-        // var town = "Addis Ababa";
-        // var nationality = "Ethiopia";
-        // var parentEmail = "birukKassaw@gmail.com";
-        // var parentPhoneNo = "0930995547";
-        // var grade = "5";
-        // var section = "1";
-        // var password = "4523"
 
 
+const register = document.getElementById('submit_new_student');
 
-        console.log(register)
-        register.addEventListener('click', function (e) {
-            e.preventDefault()
-            console.log("clicked")
-            var firstName = document.getElementById("firstName").value;
-            var lastName = document.getElementById("lastName").value;
-            var dateOfBirth = document.getElementById("dateOfBirth").value;
-            var zone = document.getElementById("zone").value;
-            var woreda = document.getElementById("woreda").value;
-            var kebele = document.getElementById("kebele").value;
-            var houseNo = document.getElementById("houseNo").value;
-            var poBox = document.getElementById("poBox").value;
-            var town = document.getElementById("town").value;
-            var nationality = document.getElementById("nationality").value;
-            var parentEmail = document.getElementById("parentEmail").value;
-            var parentPhoneNo = document.getElementById("parentPhoneNo").value;
-            var grade = document.getElementById("grade").value;
-            var section = "1";
-            var password = Math.floor(1000 + Math.random() * 9000).toString();
-            console.log(firstName)
-
-
-            // inserting id and empity grades to grades table
+register.addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("clicked")
+    var id = "ha/" + Math.floor(1000 + Math.random() * 9000).toString() + "/21"; 
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var dateOfBirth = document.getElementById("dateOfBirth").value;
+    var zone = document.getElementById("zone").value;
+    var woreda = document.getElementById("woreda").value;
+    var kebele = document.getElementById("kebele").value;
+    var houseNo = document.getElementById("houseNo").value;
+    var poBox = document.getElementById("poBox").value;
+    var town = document.getElementById("town").value;
+    var nationality = document.getElementById("nationality").value;
+    var parentEmail = document.getElementById("parentEmail").value;
+    var parentPhoneNo = document.getElementById("parentPhoneNo").value;
+    var grade = document.getElementById("grade").value;
+    var password = Math.floor(1000 + Math.random() * 9000).toString();
+    console.log(firstName)
 
 
-
-            insertStudent(db, {
-                firstName: firstName,
-                lastName: lastName,
-                dateOfBirth: dateOfBirth,
-                zone: zone,
-                woreda: woreda,
-                kebele: kebele,
-                houseNo: houseNo,
-                poBox: poBox,
-                town: town,
-                nationality: nationality,
-                parentEmail: parentEmail,
-                parentPhoneNo: parentPhoneNo,
-                grade: grade,
-                section: section,
-                password: password
-            });
-
-            // window.location.open("index.html");
-
-
-        });
-
-        // creating grades table
-            // creating grades table
-            // creating grades table
-            var grade = {
-                id: id, 
-                grade: 8,
-                grades: [
-                    // {teacherId: "1", subject: "English", grade: "90"},
-                    // {teacherId: "2", subject: "Amharic", grade: "92"},
-                    // {teacherId: "3", subject: "Biology", grade: "98"},
-                    // {teacherId: "4", subject: "Physics", grade: "89"},
-                    // {teacherId: "5", subject: "Chemistry", grade: "98"},
-                    // {teacherId: "6", subject: "Mathematics", grade: "90"},
-                    // {teacherId: "7", subject: "It", grade: "100"},
-                    // {teacherId: "8", subject: "Geography", grade: "80"},
-                    // {teacherId: "9", subject: "History", grade: "99"}
-                ]
-            }
-    
-            insertGrade(db, grade);
-    
-            // creating grades table
-            // creating grades table
-            // creating grades table
-
-    };
-
-    function insertStudent(db, student) {
-        // create a new transaction
-        const txn = db.transaction('Students', 'readwrite');
-    
-        // get the Contacts object store
-        const store = txn.objectStore('Students');
-        //
-        let query = store.put(student);
-    
-        // handle success case
-        query.onsuccess = function (event) {
-            console.log(student);
-        };
-    
-        // handle the error case
-        query.onerror = function (event) {
-            console.log(event.target.errorCode);
-        }
-
-    
-        // close the database once the 
-        // transaction completes
-
+    var studentToRegister = {
+        id: id,
+        firstName: firstName,
+        lastName: lastName,
+        dateOfBirth: dateOfBirth,
+        zone: zone,
+        woreda: woreda,
+        kebele: kebele,
+        houseNo: houseNo,
+        poBox: poBox,
+        town: town,
+        nationality: nationality,
+        parentEmail: parentEmail,
+        parentPhoneNo: parentPhoneNo,
+        grade: grade,
+        password: password
     }
 
+    studentGrade = {
+        studentId: id,
+        studentGrade: grade,
+        thisSemesterGrades: [{subject: "Maths", teacherId: "", value: "90"},
+                             {subject: "English", teacherId: "", value: "90"},  
+                             {subject: "Physics", teacherId: "", value: "78"},  
+                             {subject: "Biology", teacherId: "", value: "99"},  
+                             {subject: "Chemistry", teacherId: "", value: "89"},  
+                             {subject: "Social study", teacherId: "", value: "97"},  
+                             {subject: "Civics", teacherId: "", value: "93"},  
+                             {subject: "French", teacherId: "", value: "100"},  
+                             {subject: "Sport", teacherId: "", value: "100"},  
+                            ],
+        previousSemesterGrades: [
+                                    {
+                                        semester: "Acadamic year 2019 second semester",
+                                        grades: [
+                                            {subject: "Maths", teacherId: "", value: "90"},
+                                            {subject: "English", teacherId: "", value: "90"},  
+                                            {subject: "Physics", teacherId: "", value: "78"},  
+                                            {subject: "Biology", teacherId: "", value: "99"},  
+                                            {subject: "Chemistry", teacherId: "", value: "89"},  
+                                            {subject: "Social study", teacherId: "", value: "97"},  
+                                            {subject: "Civics", teacherId: "", value: "93"},  
+                                            {subject: "French", teacherId: "", value: "100"},  
+                                            {subject: "Sport", teacherId: "", value: "100"},  
+                                        ]
+                                    },
+                                    {
+                                        semester: "Acadamic year 2019 first semester",
+                                        grades: [
+                                            {subject: "Maths", teacherId: "", value: "90"},
+                                            {subject: "English", teacherId: "", value: "90"},  
+                                            {subject: "Physics", teacherId: "", value: "78"},  
+                                            {subject: "Biology", teacherId: "", value: "99"},  
+                                            {subject: "Chemistry", teacherId: "", value: "89"},  
+                                            {subject: "Social study", teacherId: "", value: "97"},  
+                                            {subject: "Civics", teacherId: "", value: "93"},  
+                                            {subject: "French", teacherId: "", value: "100"},  
+                                            {subject: "Sport", teacherId: "", value: "100"},  
+                                        ]
+                                    },
 
-    function insertGrade(db, grade) {
-        console.log("insert grade function gebtual")
-        // create a new transaction
-        const txn = db.transaction('Grades', 'readwrite');
-    
-        // get the Contacts object store
-        const store = txn.objectStore('Grades');
-        //
-        let query = store.put(grade);
-    
-        // handle success case
-        query.onsuccess = function (event) {
-            console.log(grade);
-        };
-    
-        // handle the error case
-        query.onerror = function (event) {
-            console.log(event.target.errorCode);
-        }
 
-    
 
+                                ]
+    
+    
     }
 
- })();
+    db.collection('students').add(studentToRegister);
+    db.collection('grades').add(studentGrade);
 
-//  end of plage
+    var formReplacement = document.getElementById("formReplacement");
+    formReplacement.innerHTML = `<div>
+                                    <h3>you have registered successfully</h3>
+                                    <p style="color:red;">your id is: <u>${id}</u><br>your password is: <u>${password}</u><p>
+                                    
+                                    <a href="logIn.html">Log In to Your account</a>
+                                </div>`
 
+    console.log(studentToRegister);
+
+    // insert empty grades row on grades table withe the registered student id
+});
--- asset/js/studentPage.js
+++ asset/js/studentPage.js
@@ -3,83 +3,94 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = Number(urlParams.get('id'));
 console.log(id);
 
-(function () {
-    // all the code will be here  
-    const request = indexedDB.open('SchoolDatabase', 1);
+db.collection('students').doc({ id: "ha/8864/21" }).get().then(student => {
+    console.log(student)
+    var welcomeNameH4 = document.getElementById("welcomeNameH4");
+    welcomeNameH4.innerHTML = "Wellcome "+student.firstName + " " + student.lastName
 
-    request.onerror = (event) => {
-        console.error(`Database error: ${event.target.errorCode}`);
-    };
+    var idDisplay = document.getElementById("id");
+    idDisplay.innerHTML = student.id
+
+    var firstName = document.getElementById("firstName");
+    firstName.value = student.firstName;
+
+    var lastName = document.getElementById("lastName");
+    lastName.value = student.lastName;
+
+    var dateOfBirth = document.getElementById("dateOfBirth");
+    dateOfBirth.value = student.dateOfBirth;
+
+    var zone = document.getElementById("zone");
+    zone.value = student.zone;
+
+    var woreda = document.getElementById("woreda");
+    woreda.value = student.woreda;
+
+    var kebele = document.getElementById("kebele");
+    kebele.value = student.kebele;
+
+    var poBox = document.getElementById("poBox");
+    poBox.value = student.poBox;
+
+    var studentSaveChanges = document.getElementById("studentSaveChanges")
+    studentSaveChanges.addEventListener("click", function (e) {
+    });
+
+    var thisSemesterGrades = student.thisSemesterGrades;
+
+  })
+
+
+// (function () {
+//     // all the code will be here  
+//     const request = indexedDB.open('SchoolDatabase', 1);
+
+//     request.onerror = (event) => {
+//         console.error(`Database error: ${event.target.errorCode}`);
+//     };
     
-    // create the Contacts object store and indexes
-    request.onupgradeneeded = (event) => {
-        let db = event.target.result;
+//     // create the Contacts object store and indexes
+//     request.onupgradeneeded = (event) => {
+//         let db = event.target.result;
 
-    };
+//     };
 
-    request.onsuccess = (event) => {
+//     request.onsuccess = (event) => {
 
-        var grade = {
-            id: 49, 
-            grades: [
-                {teacherId: "5", subject: "English", grade: "A"},
-                {teacherId: "6", subject: "Amharic", grade: "A"},
-                {teacherId: "7", subject: "Biology", grade: "A"},
-                {teacherId: "8", subject: "Physics", grade: "A"},
-                {teacherId: "9", subject: "Chemistry", grade: "A"},
-                {teacherId: "10", subject: "Mathematics", grade: "A"},
-                {teacherId: "11", subject: "It", grade: "A"},
-                {teacherId: "12", subject: "Geography", grade: "A"},
-                {teacherId: "13", subject: "History", grade: "A"}
-            ]
-        }
+//         var grade = {
+//             id: 49, 
+//             grades: [
+        //         {teacherId: "5", subject: "English", grade: "A"},
+        //         {teacherId: "6", subject: "Amharic", grade: "A"},
+        //         {teacherId: "7", subject: "Biology", grade: "A"},
+        //         {teacherId: "8", subject: "Physics", grade: "A"},
+        //         {teacherId: "9", subject: "Chemistry", grade: "A"},
+        //         {teacherId: "10", subject: "Mathematics", grade: "A"},
+        //         {teacherId: "11", subject: "It", grade: "A"},
+        //         {teacherId: "12", subject: "Geography", grade: "A"},
+        //         {teacherId: "13", subject: "History", grade: "A"}
+        //     ]
+        // }
 
-        // add implementation here
-        const db = event.target.result;
+        // // add implementation here
+        // const db = event.target.result;
 
-            const txn = db.transaction('Students', 'readonly');
-            const store = txn.objectStore('Students');
-            let query = store.get(id);
+        //     const txn = db.transaction('Students', 'readonly');
+        //     const store = txn.objectStore('Students');
+        //     let query = store.get(id);
     
-            query.onsuccess = (event) => {
+        //     query.onsuccess = (event) => {
                 
-                if (!event.target.result) {
-                    console.log(`The student with ${id} not found`);
-                } else {
-                    var student = event.target.result;
-                    console.log(student.firstName + ' ' + student.lastName+"from the student page");
-                    // displaying the student result goes here
-                    var welcomeNameH4 = document.getElementById("welcomeNameH4");
-                    welcomeNameH4.innerHTML = "Wellcome "+student.firstName + " " + student.lastName
-
-                    var id = document.getElementById("id");
-                    id.innerHTML = student.id
-
-                    var firstName = document.getElementById("firstName");
-                    firstName.value = student.firstName;
-
-                    var lastName = document.getElementById("lastName");
-                    lastName.value = student.lastName;
-
-                    var dateOfBirth = document.getElementById("dateOfBirth");
-                    dateOfBirth.value = student.dateOfBirth;
-
-                    var zone = document.getElementById("zone");
-                    zone.value = student.zone;
-
-                    var woreda = document.getElementById("woreda");
-                    woreda.value = student.woreda;
-
-                    var kebele = document.getElementById("kebele");
-                    kebele.value = student.kebele;
-
-                    var poBox = document.getElementById("poBox");
-                    poBox.value = student.poBox;
-                }
-            };
+        //         if (!event.target.result) {
+        //             console.log(`The student with ${id} not found`);
+        //         } else {
+                    // var student = event.target.result;
+                    // console.log(student.firstName + ' ' + student.lastName+"from the student page");
+                    
+            // };
         
-            query.onerror = (event) => {
-                console.log(event.target.errorCode);
-            }
-    };
- })();
+//             query.onerror = (event) => {
+//                 console.log(event.target.errorCode);
+//             }
+//     };
+//  })();
